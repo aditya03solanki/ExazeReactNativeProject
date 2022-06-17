4b0a6dcb0e0635ea170a72aa037234d9f42967fc
adityasolanki03 <adityasolankiaj03@gmail.com>
2022-06-17 11:18:36 +0530

    flow integratiion

--- src/screens/LandingScreen.js
+++ src/screens/LandingScreen.js
@@ -1,3 +1,4 @@
+//@flow
 import React, {useEffect, useState} from 'react';
 import {View, Text, StyleSheet, TextInput, NativeModules} from 'react-native';
 import NavigationService from '../navigation/NavigationService';
