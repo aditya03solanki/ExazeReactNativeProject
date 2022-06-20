fbc8dbcd9e04fd22633d5e6c85524b7b324e79d1
adityasolanki03 <adityasolankiaj03@gmail.com>
2022-06-20 19:53:22 +0530

    minor change

--- __tests__/LandingScreen-test.js
+++ __tests__/LandingScreen-test.js
@@ -4,11 +4,17 @@
 
 import 'react-native';
 import React from 'react';
+import LandingScreen from '../src/screens/landingScreen';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-import LandingScreen from '../src/screens/landingScreen';
+import {Provider} from 'react-redux';
+import {store} from '../src/redux/store';
 
 it('LandingScreen render', () => {
-  renderer.create(<LandingScreen />);
+  renderer.create(
+    <Provider store={store}>
+      <LandingScreen />
+    </Provider>,
+  );
 });
