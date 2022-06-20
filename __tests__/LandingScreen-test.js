8f2c57e5667b26847c4ab38082be67082c27d053
adityasolanki03 <adityasolankiaj03@gmail.com>
2022-06-20 18:54:38 +0530

    unit test using jest for one screen

---
+++ __tests__/LandingScreen-test.js
@@ -0,0 +1,14 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+import LandingScreen from '../src/screens/landingScreen';
+
+it('LandingScreen render', () => {
+  renderer.create(<LandingScreen />);
+});
